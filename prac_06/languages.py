7817dda729807373f2419f891722671a452e8b49
Jiajun Jiang <jjj0929@outlook.com>
2024-10-30 21:32:43 +0800

    The pseudocode and docstring of the two files of the programming language are completed.

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,8 +1,25 @@
+"""
+Languages
+Estimate: 20 minutes
+Actual:   28 minutes
+
+import the ProgrammingLanguage from programming_language
+python = in ProgrammingLanguage, language = "Python", typing = "Dynamic", reflection = True, year = 1991
+display python
+languages = [
+    in ProgrammingLanguage, language = "Python", typing = "Dynamic", reflection = True, year = 1991
+    in ProgrammingLanguage language = "Ruby", typing = "Dynamic", reflection = True, year = 1995
+    in ProgrammingLanguage language = "Visual Basic", typing = "Static", reflection = False, year = 1991
+]
+display message
+for ProgrammingLanguage in languages
+    if reflection part in ProgrammingLanguage equal to True
+    display current language in ProgrammingLanguage
+"""
+
 from prac_06.programming_language import ProgrammingLanguage
 
 python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 print(python)
 
 languages = [
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,18 +1,23 @@
 """
 Programming language
-Estimate:  minutes
-Actual:    minutes
+Estimate: 20 minutes
+Actual:   15 minutes
 """
 class ProgrammingLanguage:
+    """Represent a ProgramLanguage object"""
+
     def __init__(self, language, typing, reflection, year):
+        """Initialise a ProgramLanguage instance."""
         self.language = language
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
+        """Check the typing equal to dynamic or not then return Ture if is dynamic"""
         return self.typing == 'dynamic'
 
     def __str__(self):
+        """Set up a format for output"""
         return f"{self.language}, {self.typing} Typing, Reflection = {self.reflection}, First append in {self.year}"
 
