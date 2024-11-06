82aa09d672916bf8b19d3c4661530724290f6ee3
Jiajun Jiang <jjj0929@outlook.com>
2024-11-06 23:33:33 +0800

    Commit project.txt for easy recover and commit project.py v1

---
+++ prac_07/project.py
@@ -0,0 +1,20 @@
+"""
+Project
+Estimate: minutes
+Actual:   minutes
+"""
+
+class Project:
+    def __init__(self, name, date, priority, cost, completion_percentage):
+        self.name = name
+        self.date = date
+        self.priority = priority
+        self.cost = cost
+        self.completion_percentage = completion_percentage
+
+    def is_complete(self):
+        return self.completion_percentage == 100
+
+    def __str__(self):
+        return (f"{self.name}, start: {self.date}, priority {self.priority}, estimate: {self.cost}, "
+                f"completion: {self.completion_percentage}%")
