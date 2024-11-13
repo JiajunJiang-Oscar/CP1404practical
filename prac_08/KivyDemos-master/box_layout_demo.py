caa8a0c8b4b4eb8cff7ac384caac5870fbab2c88
Jiajun Jiang <jjj0929@outlook.com>
2024-11-14 00:19:03 +0800

    Add docstring for box_layout_demo.py

--- prac_08/KivyDemos-master/box_layout_demo.py
+++ prac_08/KivyDemos-master/box_layout_demo.py
@@ -3,16 +3,21 @@ from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    """BoxLayoutDemo is a kivy app for get name and display greet message"""
+
     def build(self):
+        """Build the app from the kv file"""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     # Update for prac_08
     def handle_greet(self):
+        """Handle greet message (call by button press), return result with input name"""
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """Handle clear button (call by button press), clean the input label"""
         self.root.ids.output_label.text = ""
         self.root.ids.input_name.text = ""
 
